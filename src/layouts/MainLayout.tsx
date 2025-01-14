8856208f5c3b710873139a205dc4cdab04585fde
nengleangkhim <leangkhimneng11@gmail.com>
2025-01-14 16:01:04 +0700

    init

---
+++ src/layouts/MainLayout.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Navbar from './Navbar'
+
+const MainLayout = () => {
+  return (
+    <div className='p-6 px-32'>
+      <Navbar/>
+    </div>
+  )
+}
+
+export default MainLayout
